1a082685bd726804dfddfda3bdaa52c303343422
Paul Connell <paulconnell@fastmail.fm>
2022-10-24 13:38:03 +0100

    Initial commit.

---
+++ src/tree.ts
@@ -0,0 +1,62 @@
+import { strictEqual } from 'assert';
+
+const Node = (operator: any, value: any, left: any, right: any) => {
+  const result = function () {
+    switch (operator) {
+      case "+":
+        return left.result() + right.result();
+      case "-":
+        return left.result() - right.result();
+      case "x":
+        return left.result() * right.result();
+      case "÷":
+        return left.result() / right.result();
+      default:
+        return value;
+    }
+  };
+
+  const toString = function () {
+    switch (operator) {
+      case "+":
+        return `(${left.toString()} + ${right.toString()})`;
+      case "-":
+        return `(${left.toString()} - ${right.toString()})`;
+      case "x":
+        return `(${left.toString()} x ${right.toString()})`;
+      case "÷":
+        return `(${left.toString()} ÷ ${right.toString()})`;
+      default:
+        return value.toString();
+    }
+  };
+
+  return {
+    operator,
+    value,
+    left,
+    right,
+    result,
+    toString
+  };
+};
+
+const tree = Node(
+  "÷",
+  null,
+  Node(
+    "+",
+    null,
+    Node("", 7, null, null),
+    Node(
+      "x",
+      null,
+      Node("-", null, Node("", 3, null, null), Node("", 2, null, null)),
+      Node("", 5, null, null)
+    )
+  ),
+  Node("", 6, null, null)
+);
+
+strictEqual("((7 + ((3 - 2) x 5)) ÷ 6)", tree.toString());
+strictEqual(2, tree.result());
